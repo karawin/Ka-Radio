b728e3e9f8ef751eb600ff4618ce87bb0b331bee
karawin <jp@cocatrix.fr>
2016-07-29 14:30:48 +0200

    Increas spi clock for external ram
    restore spi speed before flash read/write

--- ESP8266-Firmware/app/user/flash.c
+++ ESP8266-Firmware/app/user/flash.c
@@ -1,6 +1,6 @@
 #include "flash.h"
 #include "esp_common.h"
-
+#include "spi.h"
 
 
 ICACHE_FLASH_ATTR uint32_t flashRead( void *to, uint32_t fromaddr, uint32_t size )
@@ -8,6 +8,7 @@ ICACHE_FLASH_ATTR uint32_t flashRead( void *to, uint32_t fromaddr, uint32_t size
   fromaddr -= INTERNAL_FLASH_START_ADDRESS;
   int r;
   WRITE_PERI_REG(0x60000914, 0x73);
+  spi_clock(HSPI, 4, 10); //2MHz
   r = spi_flash_read(fromaddr, (uint32 *)to, size);
   if(0 == r)
     return size;
@@ -21,6 +22,7 @@ ICACHE_FLASH_ATTR uint32_t flashWrite( void *data, uint32_t fromaddr, uint32_t s
   fromaddr -= INTERNAL_FLASH_START_ADDRESS;
   int r;
   WRITE_PERI_REG(0x60000914, 0x73);
+  spi_clock(HSPI, 4, 10); //2MHz
   r = spi_flash_write(fromaddr, (uint32 *)data, size);
   if(0 == r)
     return size;
