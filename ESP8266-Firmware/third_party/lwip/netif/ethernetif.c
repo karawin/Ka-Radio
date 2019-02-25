fd314a4a0d6d03b06cf0e8a98d1c84a5a8f3d457
karawin <jp@cocatrix.fr>
2019-02-25 22:17:11 +0100

    Restore ESP8266 RTOS SDK 2.0.0

--- ESP8266-Firmware/third_party/lwip/netif/ethernetif.c
+++ ESP8266-Firmware/third_party/lwip/netif/ethernetif.c
@@ -49,6 +49,10 @@
 
 #include "esp_common.h"
 
+#ifdef MEMLEAK_DEBUG
+static const char mem_debug_file[] ICACHE_RODATA_ATTR STORE_ATTR = __FILE__;
+#endif
+
 /* Define those to better describe your network interface. */
 #define IFNAME0 'e'
 #define IFNAME1 'n'
@@ -65,11 +69,9 @@ bool default_hostname = 1;
  */
 static void
 low_level_init(struct netif *netif)
-{  
+{
   /* set MAC hardware address length */
   netif->hwaddr_len = ETHARP_HWADDR_LEN;
-
-  /* set MAC hardware address */
 
   /* maximum transfer unit */
   netif->mtu = 1500;
@@ -105,7 +107,7 @@ low_level_output(struct netif *netif, struct pbuf *p)
 {
   struct ethernetif *ethernetif = netif->state;
   struct pbuf *q;
-
+  err_t err = ERR_OK;
 //  initiate transfer();
   
 #if ETH_PAD_SIZE
@@ -115,14 +117,17 @@ low_level_output(struct netif *netif, struct pbuf *p)
   struct pbuf *tmp;
   for(q = p; q != NULL; q = q->next) {
     /* Send the data from the pbuf to the interface, one pbuf at a
-       time. The size of the data in each pbuf is kept in the ->len
-       variable. */
-//    send data from(q->payload, q->len);
+     time. The size of the data in each pbuf is kept in the ->len
+     variable. */
+  //    send data from(q->payload, q->len);
     tmp = q->next;
 
-    ieee80211_output_pbuf(netif, q);
+    err = ieee80211_output_pbuf(netif, q);
+    if (err == ERR_MEM) {
+        err = ERR_OK;
+    }
     q->next = tmp;
-	break;
+    break;
   }
 
 //  signal that packet should be sent();
@@ -133,7 +138,7 @@ low_level_output(struct netif *netif, struct pbuf *p)
   
   LINK_STATS_INC(link.xmit);
 
-  return ERR_OK;
+  return err;
 }
 
 /**
@@ -151,23 +156,23 @@ ethernetif_input(struct netif *netif, struct pbuf *p)
   struct ethernetif *ethernetif;
   struct eth_hdr *ethhdr;
 
+  if(p == NULL)
+    goto _exit;
 
-if(p == NULL)
-	goto _exit;
-
-
-if(p->payload == NULL)
-{
+  if(p->payload == NULL) {
     pbuf_free(p);
-	goto _exit;
-}
+    goto _exit;
+  }
 
+  if(netif == NULL) {
+    goto _exit;
+  }
 
-if(netif == NULL)
-{
-	goto _exit;
-}
-
+  if (!(netif->flags & NETIF_FLAG_LINK_UP)) {
+    pbuf_free(p);
+    p = NULL;
+    goto _exit;
+  }
 
   ethernetif = netif->state;
 
@@ -217,6 +222,16 @@ err_t ethernetif_init(struct netif *netif)
 {
   LWIP_ASSERT("netif != NULL", (netif != NULL));
 
+  u8_t mac[NETIF_MAX_HWADDR_LEN];
+
+  /* set MAC hardware address */
+  if ((struct netif *)wifi_get_netif(STATION_IF) == netif) {
+    wifi_get_macaddr(STATION_IF, mac);
+  } else {
+    wifi_get_macaddr(SOFTAP_IF, mac);
+  }
+  memcpy(netif->hwaddr, mac, NETIF_MAX_HWADDR_LEN);
+
 #if LWIP_NETIF_HOSTNAME
   if ((struct netif *)wifi_get_netif(STATION_IF) == netif) {
       if (default_hostname == 1) {
@@ -248,7 +263,10 @@ err_t ethernetif_init(struct netif *netif)
   netif->output_ip6 = ethip6_output;
 #endif /* LWIP_IPV6 */
   netif->linkoutput = low_level_output;
-  
+
+  extern void wifi_station_dhcpc_event(void);
+  netif->dhcp_event = wifi_station_dhcpc_event;
+
   /* initialize the hardware */
   low_level_init(netif);
 
